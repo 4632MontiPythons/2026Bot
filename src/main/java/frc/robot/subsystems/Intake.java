8a92eca5909d7d3d9c61491bbc3100bce8111778
dev <arnavakashgupta@gmail.com>
2026-03-21 22:05:55 -0500

    minisucule cleanup

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -11,9 +11,8 @@ import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.kIntake;
-
 import static frc.robot.Constants.kIntake;
+
 /**
  * It deploys immediately on auto enabled.
  * 
